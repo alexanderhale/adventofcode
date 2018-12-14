4ffa233bf83044ca8423a7dc313e914c8571a540
Alex Hale <a.hale@live.ca>
2018-12-14 13:33:33 -0500

    Day 5 complete

--- Day5.java
+++ Day5.java
@@ -15,18 +15,42 @@ public class Day5 {
             System.exit(1);
         }
 
+        System.out.println("part 1: " + fullReductionFinalLength(input));
+
+        ArrayList<Integer> shortestResults = new ArrayList<>(26);
+        for (int i = 0; i < 26; i++) {
+            shortestResults.add(fullReductionFinalLength(removeLetter(input, i)));
+        }
+
+        System.out.println("part 2: " + Collections.min(shortestResults));
+    }
+
+    public int fullReductionFinalLength(String in) {
         boolean changeMade = true;
         while (changeMade) {
             changeMade = false;
-            for (int i = 0; i < input.length() - 1; i++) {
-                if (input.charAt(i) == input.charAt(i+1) + 32 || input.charAt(i) == input.charAt(i+1) - 32) {
-                    input = input.substring(0, i) + input.substring(i+2);
+            for (int i = 0; i < in.length() - 1; i++) {
+                if (in.charAt(i) == in.charAt(i+1) + 32 || in.charAt(i) == in.charAt(i+1) - 32) {
+                    in = in.substring(0, i) + in.substring(i + 2);
                     changeMade = true;
                 }
             }
         }
+        return in.length();
+    }
 
-        System.out.println(input.length());
+    public String removeLetter(String in, int letter) {
+        boolean changeMade = true;
+        while (changeMade) {
+            changeMade = false;
+            for (int i = 0; i < in.length(); i++) {
+                if (in.charAt(i) == 65 + letter || in.charAt(i) == 97 + letter) {
+                    in = in.substring(0, i) + in.substring(i + 1);
+                    changeMade = true;
+                }
+            }
+        }
+        return in;
     }
     public static void main(String[] args) {
         try {
