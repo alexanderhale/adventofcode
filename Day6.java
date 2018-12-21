85bc46fd4f5d00222d8c1b8aa7118f5036d7a1c4
Alex Hale <a.hale@live.ca>
2018-12-20 22:01:10 -0500

    Day 6 complete

--- Day6.java
+++ Day6.java
@@ -6,16 +6,19 @@ public class Day6 {
         private Coordinate nearest;
         private int lowestDist;
         private boolean tiedNearestCoords;
+        private int totalDistance;
 
         public GridSlot(int i, int j, Coordinate c) {
             this.lowestDist = Integer.MAX_VALUE;
-            this.nearest = new Coordinate("-1000, -1000", '.');
+            this.nearest = new Coordinate("-1000, -1000");
             this.tiedNearestCoords = false;
+            this.totalDistance = 0;
             this.add(i, j, c);
         }
 
         public void add(int i, int j, Coordinate c) {
             int distance = Math.abs(i - c.getX()) + Math.abs(j - c.getY());
+            this.totalDistance += distance;
             if (distance == this.lowestDist) {
                 this.tiedNearestCoords = true;
             } else if (distance < this.lowestDist) {
@@ -28,37 +31,31 @@ public class Day6 {
         public Coordinate getClosest() {
             // if 2 or more coordinates tie for closest, it isn't added to either of their tallies
             if (this.tiedNearestCoords) {
-                return (new Coordinate("-1000, -1000", '.'));
+                return (new Coordinate("-1000, -1000"));
             } else {
                 this.nearest.incrementNumClosest();
                 return nearest;
             }
         }
+
+        public int getTotalDistance() {
+            return this.totalDistance;
+        }
     }
 
     private class Coordinate {
-        private String fullData;
-        private Character label;
         private int x;
         private int y;
         private boolean infinite;
         private int numClosest;
 
-        public Coordinate(String data, Character label) {
-            this.fullData = data;
-            this.label = label;
-            this.x = Integer.parseInt(fullData.split(", ")[0]);
-            this.y = Integer.parseInt(fullData.split(", ")[1]);
+        public Coordinate(String data) {
+            this.x = Integer.parseInt(data.split(", ")[0]);
+            this.y = Integer.parseInt(data.split(", ")[1]);
             this.infinite = false;
             this.numClosest = 0;
         }
 
-        public String getFull() {
-            return this.fullData;
-        }
-        public Character getLabel() {
-            return this.label;
-        }
         public int getX() {
             return this.x;
         }
@@ -83,24 +80,17 @@ public class Day6 {
     public void run(String[] args) {
         ArrayList<Coordinate> coords = new ArrayList<Coordinate>();
         int[] b = new int[4];       // grid boundaries
-            // b[0] = min x, b[1] = max x, b[2] = min y, b[3] = max y
-            b[0] = Integer.MAX_VALUE;
-            b[1] = Integer.MIN_VALUE;
-            b[2] = Integer.MAX_VALUE;
-            b[3] = Integer.MIN_VALUE;
-        Coordinate blank = new Coordinate("-1000, -1000", '.');
+        b[0] = Integer.MAX_VALUE;   // min x
+        b[1] = Integer.MIN_VALUE;   // max x
+        b[2] = Integer.MAX_VALUE;   // min y
+        b[3] = Integer.MIN_VALUE;   // max y
 
         try {
             Scanner f = new Scanner(new File("day6input.txt"));
-            Character label = 'A';
 
             while (f.hasNextLine()) {
-                Coordinate c = new Coordinate(f.nextLine(), label);
+                Coordinate c = new Coordinate(f.nextLine());
                 coords.add(c);
-                label++;
-                if (label == 'Z' + 1) {
-                    label = 'a';        // handle up to 52 coordinates
-                }
 
                 int x = c.getX();
                 int y = c.getY();
@@ -124,9 +114,9 @@ public class Day6 {
 
         int width = b[1] - b[0] + 2;
         int height = b[3] - b[2] + 2;
-
         GridSlot[][] grid = new GridSlot[width][height];
-        
+
+        // calculate distance to each coordinate from each slot
         for (Coordinate c : coords) {
             for (int i = 0; i < width; i++) {
                 for (int j = 0; j < height; j++) {
@@ -140,44 +130,36 @@ public class Day6 {
             }
         }
 
-        // // printer for testing
-        // for (int j = 0; j < height; j++) {
-        //     String line = "";
-        //     for (int i = 0; i < width; i++) {
-        //         Coordinate closest = grid[i][j].getClosest();
-        //         if (closest.getX() == -1000 && closest.getY() == -1000) {
-        //             closest = blank;    // period for tied squares
-        //         }
-        //         line += closest.getLabel();
-        //     }
-        //     System.out.println(line);
-        // }
-
-        // find infinite areas (i.e. areas that touch the border)
-        for (int i = 0; i < grid.length; i++) {
-            // top and bottom edges
-                // it's ok that getClosest() is called again here because
-                // it's set to infinite -> numClosest value isn't read
-            grid[i][0].getClosest().setInfinite(true);
-            grid[i][grid.length - 1].getClosest().setInfinite(true);
+        // get the closest coordinate to each grid slot
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (j == 0 || j == height - 1 || i == 0 || i == width - 1) {
+                    grid[i][j].getClosest().setInfinite(true);
+                } else {
+                    grid[i][j].getClosest();
+                }
+            }
         }
 
-        for (int j = 0; j < grid[0].length; j++) {
-            // left and right edges
-                // it's ok that getClosest() is called again here because
-                // it's set to infinite -> numClosest value isn't read
-            grid[0][j].getClosest().setInfinite(true);
-            grid[grid.length - 1][j].getClosest().setInfinite(true);
-        }
-
+        // part 1: find the largest area
         int largestArea = 0;
         for (Coordinate c : coords) {
             if (c.getNumClosest() > largestArea && c.getInfinite() == false) {
                 largestArea = c.getNumClosest();
             }
         }
-
         System.out.println(largestArea);
+
+        // part 2: find the number of slots with accumulated distance < 10000
+        int slotsBelowTenThousand = 0;
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (grid[i][j].getTotalDistance() < 10000) {
+                    slotsBelowTenThousand++;
+                }
+            }
+        }
+        System.out.println(slotsBelowTenThousand);
     }
     public static void main(String[] args) {
         try {
